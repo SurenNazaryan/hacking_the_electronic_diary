465f2ae71c02045ce7e3243272fe73212d0a723b
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2025-02-22 00:48:46 +0300

    global update

--- db_hack.py
+++ db_hack.py
@@ -1,22 +1,47 @@
 from datacenter.models import (
     Mark, Chastisement, Schoolkid, Lesson, Subject, Commendation
 )
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 import random
+
+
+FEEDBACK_LIST = [
+    "Молодец!",
+    "Отлично!",
+    "Хорошо!",
+    "Великолепно!",
+    "Прекрасно!",
+    "Очень хороший ответ!",
+    "Талантливо!",
+    "Уже существенно лучше!",
+    "Потрясающе!",
+    "Замечательно!",
+    "Так держать!",
+    "Ты на верном пути!",
+    "Здорово!",
+    "Это как раз то, что нужно!",
+    "С каждым разом у тебя получается всё лучше!",
+    "Мы с тобой не зря поработали!",
+    "Я вижу, как ты стараешься!",
+    "Ты растешь над собой!",
+    "Теперь у тебя точно все получится!"
+]
 
 
 def get_schoolkid(student_name):
     if not student_name:
         print("Вы не ввели данные. Введите данные.")
         return None
-    schoolkids = Schoolkid.objects.filter(full_name__contains=student_name)
-    if not schoolkids.exists():
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=student_name)
+        return schoolkid
+    except ObjectDoesNotExist:
         print("Ученик не найден. Введите корректные данные.")
         return None
-    elif schoolkids.count() > 1:
-        print("Найдено несколько учеников с таким именем."
+    except MultipleObjectsReturned:
+        print("Найдено несколько учеников с таким именем. "
               "Пожалуйста, уточните запрос.")
         return None
-    return schoolkids.first()
 
 
 def fix_marks(student_name):
@@ -24,13 +49,10 @@ def fix_marks(student_name):
     if schoolkid is None:
         return
 
-    schoolkid_marks = Mark.objects.filter(
+    Mark.objects.filter(
         schoolkid=schoolkid,
         points__in=[2, 3]
-    )
-    for mark in schoolkid_marks:
-        mark.points = 5
-        mark.save()
+    ).update(points=5)
     print("Оценки исправлены.")
 
 
@@ -53,40 +75,26 @@ def create_commendation(student_name, subject_name):
             title=subject_name,
             year_of_study=schoolkid.year_of_study
         )
-    except Subject.DoesNotExist:
-        print(f"Ошибка: предмет '{subject_name}' не найден."
-              "Введите корректное название предмета")
+    except ObjectDoesNotExist:
+        print(f"Предмет '{subject_name}' не найден. "
+              "Введите корректное название предмета.")
+        return
+    except MultipleObjectsReturned:
+        print(f"Найдено несколько предметов. "
+              "Пожалуйста, уточните запрос.")
         return
 
     last_lesson = Lesson.objects.filter(
         group_letter=schoolkid.group_letter,
         year_of_study=schoolkid.year_of_study,
         subject=subject
-    ).last()
+    ).order_by("-date").last()
+    if last_lesson is None:
+        print(f"У ученика {schoolkid.full_name} нет уроков "
+              f"по предмету {subject_name}.")
 
-    feedback_list = [
-        "Молодец!",
-        "Отлично!",
-        "Хорошо!",
-        "Великолепно!",
-        "Прекрасно!",
-        "Очень хороший ответ!",
-        "Талантливо!",
-        "Уже существенно лучше!",
-        "Потрясающе!",
-        "Замечательно!",
-        "Так держать!",
-        "Ты на верном пути!",
-        "Здорово!",
-        "Это как раз то, что нужно!",
-        "С каждым разом у тебя получается всё лучше!",
-        "Мы с тобой не зря поработали!",
-        "Я вижу, как ты стараешься!",
-        "Ты растешь над собой!",
-        "Теперь у тебя точно все получится!"
-    ]
     Commendation.objects.create(
-        text=random.choice(feedback_list),
+        text=random.choice(FEEDBACK_LIST),
         created=last_lesson.date,
         schoolkid=schoolkid,
         subject=subject,
